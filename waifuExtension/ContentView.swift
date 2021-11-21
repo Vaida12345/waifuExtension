9f4ae7c35a8beea8785ecfa954b564e70b0db47f
Vaida <Fan_Vaida@iCloud.com>
2021-11-22 00:12:54 +0800

    Initial Commit

---
+++ waifuExtension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  waifuExtension
+//
+//  Created by Vaida on 11/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
