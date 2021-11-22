b56610a3d16c81601013048929c5b20d5ec323dd
Vaida <Fan_Vaida@iCloud.com>
2021-11-22 18:21:17 +0800

    sace

--- waifuExtension/ContentView.swift
+++ waifuExtension/ContentView.swift
@@ -297,25 +297,6 @@ struct ConfigurationView: View {
                     
                     let modelName = "up_\(chosenStyle)_noise\(chosenNoiceLevel)_scale2x_model"
                     
-//                case anime_noise0 = "anime_noise0_model"
-//                case anime_noise1 = "anime_noise1_model"
-//                case anime_noise2 = "anime_noise2_model"
-//                case anime_noise3 = "anime_noise3_model"
-//                case anime_scale2x = "up_anime_scale2x_model"
-//                case anime_noise0_scale2x = "up_anime_noise0_scale2x_model"
-//                case anime_noise1_scale2x = "up_anime_noise1_scale2x_model"
-//                case anime_noise2_scale2x = "up_anime_noise2_scale2x_model"
-//                case anime_noise3_scale2x = "up_anime_noise3_scale2x_model"
-//                case photo_noise0 = "photo_noise0_model"
-//                case photo_noise1 = "photo_noise1_model"
-//                case photo_noise2 = "photo_noise2_model"
-//                case photo_noise3 = "photo_noise3_model"
-//                case photo_scale2x = "up_photo_scale2x_model"
-//                case photo_noise0_scale2x = "up_photo_noise0_scale2x_model"
-//                case photo_noise1_scale2x = "up_photo_noise1_scale2x_model"
-//                case photo_noise2_scale2x = "up_photo_noise2_scale2x_model"
-//                case photo_noise3_scale2x = "up_photo_noise3_scale2x_model"
-                    
                     self.modelUsed = Model(rawValue: modelName)!
                     
                 } label: {
