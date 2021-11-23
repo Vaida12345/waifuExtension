225b710c666b7f1a40dd0c23d076d1bcf2f9ae61
Vaida <Fan_Vaida@iCloud.com>
2021-11-23 12:01:10 +0800

    save before goes parallel

--- waifuExtension/ContentView.swift
+++ waifuExtension/ContentView.swift
@@ -566,6 +566,7 @@ struct ProcessingView: View {
                                 isFinished = true
                             }
                         }
+                        
                     }
                 }
             }
