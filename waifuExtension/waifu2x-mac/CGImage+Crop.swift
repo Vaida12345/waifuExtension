0b48b3f8263a108082a3e5e105044d7c4a0c82fb
Vaida <Fan_Vaida@iCloud.com>
2021-11-23 10:32:59 +0800

    save

--- waifuExtension/waifu2x-mac/CGImage+Crop.swift
+++ waifuExtension/waifu2x-mac/CGImage+Crop.swift
@@ -16,28 +16,44 @@ extension CGImage {
         let ex_w = width % Waifu2x.block_size
         let ex_h = height % Waifu2x.block_size
         var rects: [CGRect] = []
-        for i in 0..<num_w {
-            for j in 0..<num_h {
+        
+        var i = 0
+        while i < num_w {
+            var j = 0
+            while j < num_h {
                 let x = i * Waifu2x.block_size
                 let y = j * Waifu2x.block_size
                 let rect = CGRect(x: x, y: y, width: Waifu2x.block_size, height: Waifu2x.block_size)
                 rects.append(rect)
+                
+                j += 1
             }
+            
+            i += 1
         }
+        
         if ex_w > 0 {
             let x = width - Waifu2x.block_size
-            for i in 0..<num_h {
+            
+            var i = 0
+            while i < num_h {
                 let y = i * Waifu2x.block_size
                 let rect = CGRect(x: x, y: y, width: Waifu2x.block_size, height: Waifu2x.block_size)
                 rects.append(rect)
+                
+                i += 1
             }
         }
         if ex_h > 0 {
             let y = height - Waifu2x.block_size
-            for i in 0..<num_w {
+            
+            var i = 0
+            while i < num_w {
                 let x = i * Waifu2x.block_size
                 let rect = CGRect(x: x, y: y, width: Waifu2x.block_size, height: Waifu2x.block_size)
                 rects.append(rect)
+                
+                i += 1
             }
         }
         if ex_w > 0 && ex_h > 0 {
