1f70aa6a0ab140e8fff2b9f509148b268fb35c15
Vaida <Fan_Vaida@iCloud.com>
2021-11-28 19:44:25 +0800

    save

--- waifuExtension/ContentView.swift
+++ waifuExtension/ContentView.swift
@@ -832,6 +832,7 @@ struct ProcessingView: View {
                         isProcessing = false
                         isCreatingPDF = true
                     }
+                    .disabled(!(FinderItem(at: "\(NSHomeDirectory())/Downloads/Waifu\\ Output").children?.filter({ $0.isDirectory }).isEmpty ?? false))
                     .padding(.trailing)
                     
                     Button("Show in Finder") {
