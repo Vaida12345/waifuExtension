67668d8fde0b8c4486ee02b1c2fd2eef04470336
Vaida <Fan_Vaida@iCloud.com>
2021-11-28 12:43:45 +0800

    save

--- waifuExtension/Models/Waifu2xModel.swift
+++ waifuExtension/Models/Waifu2xModel.swift
@@ -11,16 +11,20 @@ import Foundation
 struct Waifu2xModel: Equatable {
     
     let `class`: String
-    let model: MLModel
+    let name: String
     let scale: Int
     let noise: Int?
     let style: String?
     
     let block_size: Int
     
-    init(class: String, model: MLModel, stye: String?, scale: Int, noise: Int?) {
+    var model: MLModel {
+        
+    }
+    
+    init(class: String, name: String, stye: String?, scale: Int, noise: Int?) {
         self.class = `class`
-        self.model = model
+        self.name = name
         self.scale = scale
         self.noise = noise
         self.block_size = scale == 1 ? 128: 142
@@ -30,36 +34,36 @@ struct Waifu2xModel: Equatable {
     static let configuration = MLModelConfiguration()
     
     static let allModels: [Waifu2xModel] = [
-//        Waifu2xModel(class: "anime_style_art", model: try! anime_style_art_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: nil),
-//        Waifu2xModel(class: "anime_style_art", model: try! anime_style_art_noise1_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 1),
-//        Waifu2xModel(class: "anime_style_art", model: try! anime_style_art_noise2_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 2),
-//        Waifu2xModel(class: "anime_style_art", model: try! anime_style_art_noise3_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 3),
+//        Waifu2xModel(class: "anime_style_art", name: "anime_style_art_scale2", stye: "anime", scale: 2, noise: nil),
+//        Waifu2xModel(class: "anime_style_art", name: "anime_style_art_noise1_model", stye: "anime", scale: 1, noise: 1),
+//        Waifu2xModel(class: "anime_style_art", name: "anime_style_art_noise2_model", stye: "anime", scale: 1, noise: 2),
+//        Waifu2xModel(class: "anime_style_art", name: "anime_style_art_noise3_model", stye: "anime", scale: 1, noise: 3),
         
-        Waifu2xModel(class: "anime_style_art_rgb", model: try! anime_style_art_rgb_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: nil),
-        Waifu2xModel(class: "anime_style_art_rgb", model: try! anime_style_art_rgb_noise0_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 0),
-        Waifu2xModel(class: "anime_style_art_rgb", model: try! anime_style_art_rgb_noise1_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 1),
-        Waifu2xModel(class: "anime_style_art_rgb", model: try! anime_style_art_rgb_noise2_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 2),
-        Waifu2xModel(class: "anime_style_art_rgb", model: try! anime_style_art_rgb_noise3_model(configuration: configuration).model, stye: "anime", scale: 1, noise: 3),
+        Waifu2xModel(class: "anime_style_art_rgb", name: "anime_style_art_rgb_scale2", stye: "anime", scale: 2, noise: nil),
+        Waifu2xModel(class: "anime_style_art_rgb", name: "anime_style_art_rgb_noise0_model", stye: "anime", scale: 1, noise: 0),
+        Waifu2xModel(class: "anime_style_art_rgb", name: "anime_style_art_rgb_noise1_model", stye: "anime", scale: 1, noise: 1),
+        Waifu2xModel(class: "anime_style_art_rgb", name: "anime_style_art_rgb_noise2_model", stye: "anime", scale: 1, noise: 2),
+        Waifu2xModel(class: "anime_style_art_rgb", name: "anime_style_art_rgb_noise3_model", stye: "anime", scale: 1, noise: 3),
         
-        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", model: try! upconv_7_anime_style_art_rgb_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: nil),
-        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", model: try! upconv_7_anime_style_art_rgb_noise0_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: 0),
-        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", model: try! upconv_7_anime_style_art_rgb_noise1_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: 1),
-        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", model: try! upconv_7_anime_style_art_rgb_noise2_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: 2),
-        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", model: try! upconv_7_anime_style_art_rgb_noise3_scale2(configuration: configuration).model, stye: "anime", scale: 2, noise: 3),
+        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", name: "upconv_7_anime_style_art_rgb_scale2", stye: "anime", scale: 2, noise: nil),
+        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", name: "upconv_7_anime_style_art_rgb_noise0_scale2", stye: "anime", scale: 2, noise: 0),
+        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", name: "upconv_7_anime_style_art_rgb_noise1_scale2", stye: "anime", scale: 2, noise: 1),
+        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", name: "upconv_7_anime_style_art_rgb_noise2_scale2", stye: "anime", scale: 2, noise: 2),
+        Waifu2xModel(class: "upconv_7_anime_style_art_rgb", name: "upconv_7_anime_style_art_rgb_noise3_scale2", stye: "anime", scale: 2, noise: 3),
         
-        Waifu2xModel(class: "upconv_7_photo", model: try! upconv_7_photo_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: nil),
-        Waifu2xModel(class: "upconv_7_photo", model: try! upconv_7_photo_noise0_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: 0),
-        Waifu2xModel(class: "upconv_7_photo", model: try! upconv_7_photo_noise1_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: 1),
-        Waifu2xModel(class: "upconv_7_photo", model: try! upconv_7_photo_noise2_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: 2),
-        Waifu2xModel(class: "upconv_7_photo", model: try! upconv_7_photo_noise3_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: 3),
+        Waifu2xModel(class: "upconv_7_photo", name: "upconv_7_photo_scale2", stye: "photo", scale: 2, noise: nil),
+        Waifu2xModel(class: "upconv_7_photo", name: "upconv_7_photo_noise0_scale2", stye: "photo", scale: 2, noise: 0),
+        Waifu2xModel(class: "upconv_7_photo", name: "upconv_7_photo_noise1_scale2", stye: "photo", scale: 2, noise: 1),
+        Waifu2xModel(class: "upconv_7_photo", name: "upconv_7_photo_noise2_scale2", stye: "photo", scale: 2, noise: 2),
+        Waifu2xModel(class: "upconv_7_photo", name: "upconv_7_photo_noise3_scale2", stye: "photo", scale: 2, noise: 3),
         
-        Waifu2xModel(class: "photo", model: try! photo_scale2(configuration: configuration).model, stye: "photo", scale: 2, noise: nil),
-        Waifu2xModel(class: "photo", model: try! photo_noise0_model(configuration: configuration).model, stye: "photo", scale: 1, noise: 0),
-        Waifu2xModel(class: "photo", model: try! photo_noise1_model(configuration: configuration).model, stye: "photo", scale: 1, noise: 1),
-        Waifu2xModel(class: "photo", model: try! photo_noise2_model(configuration: configuration).model, stye: "photo", scale: 1, noise: 2),
-        Waifu2xModel(class: "photo", model: try! photo_noise3_model(configuration: configuration).model, stye: "photo", scale: 1, noise: 3),
+        Waifu2xModel(class: "photo", name: "photo_scale2", stye: "photo", scale: 2, noise: nil),
+        Waifu2xModel(class: "photo", name: "photo_noise0_model", stye: "photo", scale: 1, noise: 0),
+        Waifu2xModel(class: "photo", name: "photo_noise1_model", stye: "photo", scale: 1, noise: 1),
+        Waifu2xModel(class: "photo", name: "photo_noise2_model", stye: "photo", scale: 1, noise: 2),
+        Waifu2xModel(class: "photo", name: "photo_noise3_model", stye: "photo", scale: 1, noise: 3),
         
-        Waifu2xModel(class: "ukbench", model: try! ukbench_scale2(configuration: configuration).model, stye: nil, scale: 2, noise: nil)
+        Waifu2xModel(class: "ukbench", name: "ukbench_scale2", stye: nil, scale: 2, noise: nil)
     ]
 }
 
